d5816668c54c98a34845d10dc62dd398b4aedca3
John Jardin <john@agilite.io>
2020-01-06 20:34:54 +0200

    Created a custom folder with sample components and logic

---
+++ src/custom/index.js
@@ -0,0 +1,9 @@
+// We need to get a handle on the App's Event Emitter
+import { eventEmitter } from '../core/index'
+
+const handleOnAppLoad = (e) => {
+  console.log('onappload triggered in custom index', e)
+  eventEmitter.removeListener('onappload', handleOnAppLoad)
+}
+
+eventEmitter.on('onappload', handleOnAppLoad)
