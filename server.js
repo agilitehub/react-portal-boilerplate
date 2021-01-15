c8a4060289a787fcef41b2f964a02a10ba6ef89b
John Jardin <john@agilite.io>
2021-01-15 09:34:26 +0200

    Basic updates to core files includes version updates

---
+++ server.js
@@ -0,0 +1,20 @@
+'use strict'
+
+require('agilite-utils/dotenv').config()
+
+const Express = require('express')
+const Path = require('path')
+const HTTP = require('http')
+const Compression = require('compression')
+
+const app = Express()
+app.use(Compression())
+
+// Serve the files out of ./build as our main files
+app.use('/', Express.static(Path.join(__dirname, '/build')))
+app.get('/', (req, res) => { res.sendFile(Path.join(__dirname, '/build/index.html')) })
+
+// Server Setup
+const port = process.env.PORT || 80
+const server = HTTP.createServer(app)
+server.listen(port, () => { console.log(`${process.env.REACT_APP_NAME} listening on Port - ${port}`) })
